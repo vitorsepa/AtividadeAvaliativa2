e734c4664cfa61953e46d63da26aa2ac472f1180
VITOR COSTA CARDOSO DA LUZ <vitor_c_luz@estudante.sesisenai.org.br>
2025-05-07 10:55:03 -0300

    achando logs pelo id

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const { registrarLog } = require('./script');
 
 const app = express();
@@ -15,6 +16,26 @@ app.post('/logs', (req, res) => {
   return res.status(200).json({ message: 'Log registrado com sucesso.' });
 });
 
-app.listen(8000, () => {
-  console.log('ta rodando na porta 8000');
-});
+//--------------------------------------------------------------------------------
+
+app.get('/logs/:id', (req, res) => {
+    const { id } = req.params;
+    fs.readFile('logs.txt', 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Erro ao ler o arquivo de logs.' });
+      }
+  
+      const logs = data.split('\n');
+      const log = logs.find(log => log.startsWith(id));
+  
+      if (log) {
+        return res.status(200).json({ log });
+      } else {
+        return res.status(404).json({ error: 'Log não encontrado.' });
+      }
+    });
+  });
+
+  app.listen(8000, () => {
+    console.log('ta rodando na porta 8000');
+  });
